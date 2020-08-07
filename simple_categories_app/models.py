43d60060d7fd186ed939b4aabb92205951e78410
Mikhail Klimovich <mikhail12klimovich@gmail.com>
2020-08-07 03:05:33 +0300

    create model Category

--- simple_categories_app/models.py
+++ simple_categories_app/models.py
@@ -1,2 +1,6 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, blank=True, null=True)
